d33653b730cd9354a2f22a4493c65891d867211e
Bongkyu Jeon <legend59@gmail.com>
2022-06-12 20:52:16 +0900

    Add TechDoc.md and modified some cs

--- HTTP/http_server_/Program.cs
+++ HTTP/http_server_/Program.cs
@@ -5,10 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
-using System.Web;
-using System.Collections.Specialized;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace SP_TEST
 {
@@ -55,49 +51,13 @@ namespace SP_TEST
             context.Response.Close();
         }
 
-        /*
-        Request : http://127.0.0.1:8080/search?hl=en&q=parsing+a+url+in+c%23&aq=f&aqi=g1g-j9&aql=&oq=
-        The current time is 2022-06-12 오후 12:54:33
-        Protocol: http
-        Host: 127.0.0.1
-        Path: /search
-        Query: ?hl=en&q=parsing+a+url+in+c%23&aq=f&aqi=g1g-j9&aql=&oq=
-        Parms: 6
-        Parm: hl = en
-        Parm: q = parsing a url in c#
-        Parm: aq = f
-        Parm: aqi = g1g-j9
-        Parm: aql =
-        Parm: oq =
-            */
         static void reqeustDate()
         {
             var context = listener.GetContext();
             Console.WriteLine("Request : " + context.Request.Url);
-            Uri tmp = context.Request.Url;
 
             string time = DateTime.Now.ToString();// ("h:mm:ss tt");
             Console.WriteLine("The current time is {0}", time);
-
-            Console.WriteLine("Protocol: {0}", tmp.Scheme);
-            Console.WriteLine("Host: {0}", tmp.Host);
-            Console.WriteLine("Path: {0}", HttpUtility.UrlDecode(tmp.AbsolutePath));
-            Console.WriteLine("Query: {0}", tmp.Query);
-
-            // case1. 파라미터를 모를 때
-            //
-            NameValueCollection Parms = HttpUtility.ParseQueryString(tmp.Query);
-            Console.WriteLine("Parms: {0}", Parms.Count);
-            foreach (string x in Parms.AllKeys)
-                Console.WriteLine("\tParm: {0} = {1}", x, Parms[x]);
-            //
-
-            // case2. 파라미터를 알고 있을 때
-            /*
-            var Parms = HttpUtility.ParseQueryString(tmp.Query);
-            string aql = Parms["hl"];
-            Console.WriteLine("Parms for hl: {0}", aql);
-            */
 
             byte[] data = Encoding.UTF8.GetBytes(time);
             context.Response.OutputStream.Write(data, 0, data.Length);
@@ -124,55 +84,16 @@ namespace SP_TEST
             string fileName = string.Format(".\\Output\\{0:D2}{1:D2}{2:D2}.json", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
             Console.WriteLine("fileName : " + fileName);
 
-            Console.WriteLine("s : " + s);
-            JObject json_p1 = JObject.Parse(s);
-            Console.WriteLine("get type of json_p1 : {0}", json_p1.Type);
-            Console.WriteLine("profile_image : " + json_p1["profile_image"]);
-
-
-            StreamWriter sw = new StreamWriter(fileName);
+           StreamWriter sw = new StreamWriter(fileName);
             sw.WriteLine(s);
             sw.Close();
             body.Close();
             reader.Close();
 
-            strRes = MakeResposneString();
-
             byte[] data = Encoding.UTF8.GetBytes(strRes);
             context.Response.OutputStream.Write(data, 0, data.Length);
             context.Response.StatusCode = 200;
             context.Response.Close();
-        }
-
-        static string MakeResposneString ()
-        {
-            string str = "";
-
-            JObject data1 = new JObject();
-
-            data1.Add("employee_name", "ObjectTest");
-            data1.Add("profile_image", "test1.png");
-            data1.Add("employee_age", "30");
-            data1.Add("employee_salary", "11111");
-
-            // JSON Object for second employee
-            JObject data2 = new JObject();
-
-            data2.Add("employee_name", "MapTest");
-            data2.Add("profile_image", "test2.png");
-            data2.Add("employee_age", "20");
-            data2.Add("employee_salary", "99999");
-
-            // Creating JSON array to add both JSON objects
-            JArray array = new JArray();
-            array.Add(data1);
-            array.Add(data2);
-
-            str = array.ToString();
-
-            Console.WriteLine("str : " + str);
-
-            return str;
         }
     }
 }
---
+++ TechDoc.md
@@ -0,0 +1,106 @@
+# Cloud
+## 1. Kubernetes 기반 솔루션 가이드
+## 2. Serverless Computing 활용 가이드
+## 3. AWS
+ * [https://docs.aws.amazon.com]
+
+### Compute
+| 주제 | url |
+|----|----|
+| EC2 | https://docs.aws.amazon.com/ec2/?id=docs_gateway |
+| EC2 Image Builder | https://docs.aws.amazon.com/imagebuilder/?id=docs_gateway |
+| Lambda | https://docs.aws.amazon.com/imagebuilder/?id=docs_gateway |
+
+### Containers
+| 주제 | url |
+|----|----|
+| ECR | https://docs.aws.amazon.com/ecr/?id=docs_gateway |
+| ECS | https://docs.aws.amazon.com/ecs/?id=docs_gateway |
+| EKS | https://docs.aws.amazon.com/eks/?id=docs_gateway |
+| App2Container | https://docs.aws.amazon.com/app2container/?id=docs_gateway |
+
+### Networking & Content Delivery
+| 주제 | url |
+|----|----|
+| API Gateway | https://docs.aws.amazon.com/apigateway/?id=docs_gateway |
+| VPC | https://docs.aws.amazon.com/vpc/?id=docs_gateway |
+| Route 3 | https://docs.aws.amazon.com/route53/?id=docs_gateway |
+
+### Storage
+| 주제 | url |
+|----|----|
+| S3 | https://docs.aws.amazon.com/s3/?id=docs_gateway |
+| EBS | https://docs.aws.amazon.com/ebs/?id=docs_gateway |
+| EFS | https://docs.aws.amazon.com/efs/?id=docs_gateway |
+| S3 Glacier | https://docs.aws.amazon.com/s3/?id=docs_gateway |
+
+### Security, Identity, & Compliance
+| 주제 | url |
+|----|----|
+| IAM | https://docs.aws.amazon.com/iam/?id=docs_gateway |
+
+### Management & Governace
+| 주제 | url |
+|----|----|
+| CloudFormation | https://docs.aws.amazon.com/cloudformation/?id=docs_gateway |
+| CloudWatch | https://docs.aws.amazon.com/cloudwatch/?id=docs_gateway |
+| AppConfig | https://docs.aws.amazon.com/appconfig/?id=docs_gateway |
+| Auto Scaling | https://docs.aws.amazon.com/autoscaling/?id=docs_gateway |
+
+## 4. GCP
+ * [https://cloud.google.com/docs/]
+
+### 컴퓨팅
+| 주제 | url |
+|----|----|
+| Compute Engine | https://cloud.google.com/compute/docs |
+| 선점형 VM | * |
+| 보안 VM | * |
+
+### 컨테이너
+| 주제 | url |
+|----|----|
+| Google Kubernetes Engine(GKE) | https://cloud.google.com/kubernetes-engine/docs |
+| Container Registry(Artifact Registry로 전환 추세) | https://cloud.google.com/container-registry/docs |
+
+### 네트워킹
+| 주제 | url |
+|----|----|
+| Virtual Private Cloud(VPC) | https://cloud.google.com/vpc/docs |
+| Cloud Router | https://cloud.google.com/network-connectivity/docs/router |
+| Cloud NAT | https://cloud.google.com/nat/docs |
+| Cloud Load Balancing | https://cloud.google.com/load-balancing/docs |
+
+### 보안 및 ID
+| 주제 | url |
+|----|----|
+| Identity and Access Management(IAM) | https://cloud.google.com/iam/docs |
+| Resource Manager | https://cloud.google.com/resource-manager/docs |
+| 보안 VM | * |
+| VPC 서비스 제어 | https://cloud.google.com/vpc-service-controls/docs |
+| Cloud Key Management Service | https://cloud.google.com/kms/docs |
+
+### 서버리스 컴퓨팅
+| 주제 | url |
+|----|----|
+| Cloud Functions | https://cloud.google.com/functions/docs |
+| Cloud Run | https://cloud.google.com/run/docs |
+
+### 관리도구
+| 주제 | url |
+|----|----|
+| Cloud Console | * |
+| Cloud Shell | https://cloud.google.com/shell/docs |
+| 비용 관리(-> Cloud Billing) | https://cloud.google.com/billing/docs |
+| Cloud APIs | https://cloud.google.com/apis/docs/overview |
+
+## 5. Azure
+ * [https://docs.microsoft.com/en-us/learn/]
+ * [https://www.microsoft.com/handsonlabs]
+
+### Describe core Azure concepts
+### Describe core Azure services
+### Describe core solutions and management tools on Azure
+### Describe general security and network security features
+### Describe identity, governace, privacy, and compliance features
+### Describe Azure cost management and service level agreements
